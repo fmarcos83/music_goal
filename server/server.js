eff31fdd98e919570e233d820c74f2ee1a54b705
Francisco Marcos <fmarcos83@gmail.com>
2016-03-02 01:13:51 +0100

    removed local signup

--- server/server.js
+++ server/server.js
@@ -100,42 +100,6 @@ app.get('/local', function (req, res, next){
   });
 });
 
-app.get('/signup', function (req, res, next){
-  res.render('pages/signup', {
-    user: req.user,
-    url: req.url
-  });
-});
-
-app.post('/signup', function (req, res, next) {
-
-  var User = app.models.user;
-
-  var newUser = {};
-  newUser.email = req.body.email.toLowerCase();
-  newUser.username = req.body.username.trim();
-  newUser.password = req.body.password;
-
-  User.create(newUser, function (err, user) {
-    if (err) {
-      req.flash('error', err.message);
-      return res.redirect('back');
-    } else {
-      // Passport exposes a login() function on req (also aliased as logIn())
-      // that can be used to establish a login session. This function is
-      // primarily used when users sign up, during which req.login() can
-      // be invoked to log in the newly registered user.
-      req.login(user, function (err) {
-        if (err) {
-          req.flash('error', err.message);
-          return res.redirect('back');
-        }
-        return res.redirect('/auth/account');
-      });
-    }
-  });
-});
-
 app.get('/login', function (req, res, next){
   res.render('pages/login', {
     user: req.user,
